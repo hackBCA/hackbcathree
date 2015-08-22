4dc053954c942e61509672856da5853b1b8e01ba
Robert <rkty13@gmail.com>
2015-08-22 00:16:42 -0400

    added steps to readme

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
+import sys
+
 from landing import app
 from landing import DEBUG
 
-
 if __name__ == "__main__":
-    app.run(debug=DEBUG)
+    if len(sys.argv) > 1:
+        port = int(sys.argv[1])
+    else:
+        port = 5000
+    app.run(debug=DEBUG, port=port)
