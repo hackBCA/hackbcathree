07f35ac588440256b775820f346d01b83131a162
Robert Kim <rkty13@gmail.com>
2015-11-13 19:18:22 -0500

    oops, last change

--- application/mod_web/views.py
+++ application/mod_web/views.py
@@ -1,6 +1,14 @@
-from flask import render_template
+from flask import render_template, redirect
 from . import web_module as web
 
 @web.route("/")
 def index():
     return render_template("index.html")
+
+@web.route("/sponsors",methods=["GET"])
+def sponsors():
+    return web.send_static_file("sponsors.pdf")
+
+@web.route("/sponsors.pdf",methods=["GET"])
+def foward_sponsors():
+    return redirect("/sponsors")
