7051714ad24dc0bb6d450bc531b1b7fe622df99e
= <=>
2015-11-27 15:57:32 -0500

    Added email verification system for mailing list

--- application/mod_web/models.py
+++ application/mod_web/models.py
@@ -2,3 +2,4 @@ from mongoengine import *
 
 class MailingListEntry(Document):
 	email = StringField(max_length = 50, required=True)
+	verified = BooleanField(required=False, default=False)
--- application/mod_web/views.py
+++ application/mod_web/views.py
@@ -1,16 +1,24 @@
-from flask import render_template, request
+from flask import *
 from . import web_module as mod_web
-from .controllers import *
+from . import controllers as controller
 
 @mod_web.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "POST":
-        try:
-            email = request.form["email"]
-            add_email(email)
-        except KeyError:
-            pass
-    return render_template("index.html")
+	if request.method == "POST":
+		if request.form["email"]:
+			email = request.form["email"]
+			try:
+				controller.add_email(email)
+			except Exception as e:
+				if(e.args[0] == 'EmailExistsError'):
+					return render_template("index.html", error = e.args[1])
+				pass
+			return render_template("index.html", message = 'Verification email sent.')
+	if request.args.get('status'):
+		status = request.args.get('status')
+		if status == 'confirmed':
+			return render_template("index.html", message = "Subscription confirmed.")
+	return render_template("index.html")
 
 @mod_web.route("/sponsors",methods=["GET"])
 def sponsors():
@@ -19,3 +27,8 @@ def sponsors():
 @mod_web.route("/sponsors.pdf",methods=["GET"])
 def foward_sponsors():
     return redirect("/sponsors")
+
+@mod_web.route('/confirm/<token>')
+def confirm_email(token):
+	controller.confirm_email(token);
+	return redirect('/?status=confirmed');
