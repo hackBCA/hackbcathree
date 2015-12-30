4620a0948e3682a5025f547a4600eccb92287975
= <jack.neus@gmail.com>
2015-12-30 02:57:43 -0500

    Rebase fix

--- application/mod_user/models.py
+++ application/mod_user/models.py
@@ -1,24 +1,44 @@
 from flask.ext.login import LoginManager, UserMixin
 from mongoengine import *
 
+application_fields = ["school", "gender", "beginner", "ethnicity", "grade", "num_hackathons", "github_link", "linkedin_link", "site_link", "other_link", "resp1", "resp2", "resp3"]
+
 #Mongo Object
 class UserEntry(Document):
+	email = StringField(required = True)
+	hashed = StringField(required = True)
+
 	firstname = StringField(required = True)
 	lastname = StringField(required = True)	
-	email = StringField(required = True)
 
 	verified = BooleanField(required = False, default = False)
 
-	hashed = StringField(required = True)
-	def __repr__(self):
-		return ', '.join([self.firstname, self.lastname, self.email])
+	hacker = BooleanField(required = True, default = True)
+
+	school = StringField()
+	gender = StringField()
+	beginner = StringField()
+	ethnicity = StringField()
+	grade = StringField()
+	num_hackathons = StringField()
+	
+	github_link = StringField()
+	linkedin_link = StringField()
+	site_link = StringField()
+	other_link = StringField()
+
+	resp1 = StringField()
+	resp2 = StringField()
+	resp3 = StringField()	
+
 
 class User(UserMixin):
-	def __init__(self, uid, email, firstname, lastname):
+	def __init__(self, uid, email, firstname, lastname, hacker):
 		self.uid = str(uid)
 		self.email = email
 		self.firstname = firstname
 		self.lastname = lastname
+		self.hacker = hacker
 
 	def is_authenticated(self):
 		return True
