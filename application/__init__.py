220ddaa84a5ec6a23a36a10e00d6a6167fa58de4
= <jack.neus@gmail.com>
2016-01-11 09:22:51 -0500

    Added 401 error page.

--- application/__init__.py
+++ application/__init__.py
@@ -55,6 +55,9 @@ register_connection (
 # Cache
 cache = Cache(app,config={"CACHE_TYPE": "simple", "CACHE_DEFAULT_TIMEOUT": 60 * 60 * 24 * 7})
 
+@app.errorhandler(401)
+def error(e):
+    return render_template("401.html"), 401
 
 @app.errorhandler(404)
 def error(e):
