1f729dfedcfa31930e3ba74aa69890abfb1bf069
= <jack.neus@gmail.com>
2016-01-11 22:59:00 -0500

    Fixed link validation issue with http prefix.

--- application/mod_user/views.py
+++ application/mod_user/views.py
@@ -190,9 +190,12 @@ def application():
         elif "submit" in request.form:
           controller.save_application(current_user.email, request.form)
 
+          modifiedFormData = {}
+
           if form.validate():
             flash("Application Submitted", "success")
-            controller.set_user_attr(current_user.email, "status", "Submitted")
+            if not CONFIG["DEBUG"]:
+              controller.set_user_attr(current_user.email, "status", "Submitted")
             
             controller.login(current_user.email) #To immediately update application status and disable the form
           else:
