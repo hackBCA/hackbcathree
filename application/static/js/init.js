e443d208b54f5a7e617368dedcbc7976b6e8c2a0
Robert Kim <rkty13@gmail.com>
2016-03-26 18:57:59 -0400

    Fixed a long standing IE bug with RSVP.

--- application/static/js/init.js
+++ application/static/js/init.js
@@ -28,3 +28,17 @@ if (document.querySelector("#credit i")) {
         prev_icon = new_icon;
     }
 }
+
+function labelSimulateClick () {
+    var labels = document.querySelectorAll("label");
+    for (var i = 0; i < labels.length; i++) {
+        labels[i].onclick = function() {
+            var associated = this.getAttribute("for");
+            var input = document.getElementById(associated);
+            input.setAttribute("checked", "checked");
+            input.click();
+        }
+    }
+}
+
+labelSimulateClick();
