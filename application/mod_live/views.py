35de1ed950892b6430242974d58220838a2a02c8
Robert Kim <rkty13@gmail.com>
2016-03-28 23:46:49 -0400

    Added config for broadcast url.

--- application/mod_live/views.py
+++ application/mod_live/views.py
@@ -1,7 +1,8 @@
 from flask import render_template
 from flask.ext.login import login_required, current_user
 from . import live_module as mod_live
+from application import CONFIG
 
 @mod_live.route("/live", methods = ["GET", "POST"])
 def live():
-    return render_template("live.live.html")
+    return render_template("live.live.html", broadcast_url = CONFIG["BROADCAST_URL"])
