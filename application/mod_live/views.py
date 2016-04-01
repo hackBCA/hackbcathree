d07fb2122be464a5fc809bef0e0a3e3cddbc7ac2
Robert Kim <rkty13@gmail.com>
2016-04-01 00:47:57 -0400

    Added schedule to live page.

--- application/mod_live/views.py
+++ application/mod_live/views.py
@@ -2,7 +2,14 @@ from flask import render_template, request
 from flask.ext.login import login_required, current_user
 from . import live_module as mod_live
 from application import CONFIG
+from application.mod_web import controllers as web_controllers
 
 @mod_live.route("/live", methods = ["GET", "POST"])
 def live():
-    return render_template("live.live.html", broadcast_url = CONFIG["BROADCAST_URL"], session = request.cookies.get("session"))
+    schedule = web_controllers.get_schedule()
+    return render_template(
+                "live.live.html", 
+                broadcast_url = CONFIG["BROADCAST_URL"], 
+                session = request.cookies.get("session"),
+                schedule = schedule
+            )
